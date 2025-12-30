76ac014a28893d4b7540f538f39a69c34d8ebf22
nhj9609@feelanet.com <nhj9609@feelanet.com>
2025-12-30 17:51:39 +0900

    added document table list

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,17 @@
 import React from "react";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
-import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { FullscreenLoader } from "@/components/fullscreen-loader";
+import { usePaginatedQuery } from "convex/react";
+import { DocumentsTable } from "./documents-table";
 
 const Home = () => {
-  const documents = useQuery(api.documents.get);
-
-  if (documents === undefined) {
-    return <FullscreenLoader label="Loading documents..." />;
-  }
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.documents.get,
+    {},
+    { initialNumItems: 5 }
+  );
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -21,9 +22,11 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
-        {documents?.map((document) => (
-          <span key={document._id}>{document.title}</span>
-        ))}
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
       </div>
     </div>
   );
--- app/(home)/templates-gallery.tsx
+++ app/(home)/templates-gallery.tsx
@@ -9,9 +9,26 @@ import {
 } from "@/components/ui/carousel";
 import { cn } from "@/lib/utils";
 import { templates } from "@/constants/templates";
+import { useRouter } from "next/navigation";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useState } from "react";
 
 export const TemplatesGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+      .then((documentId) => {
+        router.push(`/documents/${documentId}`);
+      })
+      .finally(() => {
+        setIsCreating(false);
+      });
+  };
   return (
     <div className="bg-[#F1F3F4]">
       <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
@@ -29,7 +46,10 @@ export const TemplatesGallery = () => {
                   )}>
                   <button
                     disabled={isCreating}
-                    onClick={() => {}}
+                    onClick={() => {
+                      onTemplateClick(template.label, "");
+                      // TODO: Add Proper initial content
+                    }}
                     style={{
                       backgroundImage: `url(${template.imgUrl})`,
                       backgroundSize: "cover",
