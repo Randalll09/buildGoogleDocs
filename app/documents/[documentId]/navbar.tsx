76cf46a9dfceb0e74efdf29e0be03285ade95257
nhj9609@feelanet.com <nhj9609@feelanet.com>
2025-12-30 11:07:07 +0900

    made navbae

--- app/documents/[documentId]/navbar.tsx
+++ app/documents/[documentId]/navbar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { BsFilePdf } from "react-icons/bs";
 import { DocumentInput } from "./document-input";
 
 import {
@@ -14,7 +17,24 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { MenubarMenu } from "@radix-ui/react-menubar";
-import { FileIcon } from "lucide-react";
+import {
+  BoldIcon,
+  FileIcon,
+  FileJsonIcon,
+  FilePenIcon,
+  FilePlusIcon,
+  FileTextIcon,
+  GlobeIcon,
+  ItalicIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  StrikethroughIcon,
+  TextIcon,
+  TrashIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
 
 export const Navbar = () => {
   return (
@@ -28,11 +48,130 @@ export const Navbar = () => {
           <div className="flex">
             <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
               <MenubarMenu>
-                <MenubarTrigger>File</MenubarTrigger>
+                {/* filemenuStart */}
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  File
+                </MenubarTrigger>
+                <MenubarContent className="print:hidden">
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <FileIcon className="mr-2 size-4" />
+                      Save
+                    </MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <FileJsonIcon className="mr-2 size-4" />
+                        JSON
+                      </MenubarItem>
+                      <MenubarItem>
+                        <GlobeIcon className="mr-2 size-4" />
+                        HTML
+                      </MenubarItem>
+                      <MenubarItem>
+                        <BsFilePdf className="mr-2 size-4" />
+                        PDF
+                      </MenubarItem>
+                      <MenubarItem>
+                        <FileTextIcon className="mr-2 size-4" />
+                        Text
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarItem>
+                    <FilePlusIcon className="mr-2 size-4" />
+                    New Document
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem>
+                    <FilePenIcon className="mr-2 size-4" />
+                    Rename
+                  </MenubarItem>
+                  <MenubarItem>
+                    <TrashIcon className="mr-2 size-4" />
+                    Remove
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem onClick={() => window.print()}>
+                    <PrinterIcon className="mr-2 size-4" />
+                    Print
+                    <MenubarShortcut>{`\u2318`}P</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              {/* fileEnd  */}
+              {/* EditStart */}
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Edit
+                </MenubarTrigger>
                 <MenubarContent>
                   <MenubarItem>
-                    <FileIcon className="mr-2 size-4" />
-                    Save
+                    <Undo2Icon className="mr-2 size-4" />
+                    Undo
+                    <MenubarShortcut>{`\u2318`}Z</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    <Redo2Icon className="mr-2 size-4" />
+                    Redo
+                    <MenubarShortcut>{`\u2318`}Y</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              {/* editEnd */}
+              {/* insertStart */}
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Insert
+                </MenubarTrigger>
+                <MenubarContent>
+                  <MenubarSub>
+                    <MenubarSubTrigger>Table</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>1 x 1</MenubarItem>
+                      <MenubarItem>2 x 2</MenubarItem>
+                      <MenubarItem>3 x 3</MenubarItem>
+                      <MenubarItem>4 x 4</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Format
+                </MenubarTrigger>
+                <MenubarContent>
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <TextIcon className="mr-2 size-4" />
+                      Text
+                    </MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <BoldIcon className="mr-2 size-4" />
+                        Bold
+                        <MenubarShortcut>{`\u2318`}B</MenubarShortcut>
+                      </MenubarItem>
+                      <MenubarItem>
+                        <ItalicIcon className="mr-2 size-4" />
+                        Italic
+                        <MenubarShortcut>{`\u2318`}I</MenubarShortcut>
+                      </MenubarItem>
+                      <MenubarItem>
+                        <UnderlineIcon className="mr-2 size-4" />
+                        Underline
+                        <MenubarShortcut>{`\u2318`}U</MenubarShortcut>
+                      </MenubarItem>
+                      <MenubarItem>
+                        <StrikethroughIcon className="mr-2 size-4" />
+                        Strikethrough &nbsp;
+                        <MenubarShortcut>{`\u2318`}S</MenubarShortcut>
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarItem>
+                    <RemoveFormattingIcon className="mr-2 size-4" />
+                    Clear formatting
+                    <MenubarShortcut>{`\u2318`}X</MenubarShortcut>
                   </MenubarItem>
                 </MenubarContent>
               </MenubarMenu>
